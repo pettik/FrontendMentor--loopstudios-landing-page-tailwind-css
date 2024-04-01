f1adf228b8ada99e07b67a39e589f3881631d7b7
pettik <petrbednarski@gmail.com>
2024-04-01 19:42:36 +0200

    last html/css/js changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+const navToggle = () => {
+  menuBtn.classList.toggle('open');
+  menu.classList.toggle('hidden');
+};
+
+menuBtn.addEventListener('click', navToggle);
